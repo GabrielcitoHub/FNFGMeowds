9d9eec4a9359f73b5c7b768b29878c3cb2e6f9c9
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-04-24 23:31:14 -0300

    final bits of polishing :))

--- FNF Psych Engine/mods/mushroom-mechanic-V4/data/gameover/thescript.lua
+++ FNF Psych Engine/mods/mushroom-mechanic-V4/data/gameover/thescript.lua
@@ -86,7 +86,7 @@ function onCreate()
     precacheImage('background/pcport/loop')
     setPropertyFromClass('lime.app.Application', 'current.window.title', 'Funk Mix: Game Over')
     setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'empty')
-    initPauseMenuGui()
+    initPauseMenu()
 end
 
 function onCountdownTick(swagCounter)
@@ -96,9 +96,22 @@ function onCountdownTick(swagCounter)
     end
 end
 
+local options = {"CONTINUE","RETRY","OPTIONS","END"}
 local pauseMenuSprs = {}
+local selectedOption = 1
 
-function initPauseMenuGui()
+local function initPauseMenu()
+    precacheImage("pause/bg")
+    precacheImage("pause/pause")
+    precacheImage("pause/progressbar")
+    precacheImage("pause/stand")
+    precacheImage("pause/selector")
+
+    precacheSound("menu_select")
+    precacheSound("pause")
+    precacheSound("stomp")
+    precacheSound("coin")
+    
     makeLuaSprite('substateBG', 'pause/bg', 0, 0)
     scaleObject("substateBG", 10, 10)
     screenCenter('substateBG', 'xy')
@@ -109,20 +122,68 @@ function initPauseMenuGui()
     setProperty('pauseText.y', 42)
     setProperty('pauseText.x', getProperty("pauseText.x") -4.05)
 
-    makeLuaText('songnameText', 'GAME OVER', 200, 346.48, 183)
+    makeLuaText('songnameText', 'GAME OVER', 200, 344.48, 183)
     scaleObject("songnameText", 3, 3)
     setTextFont("songnameText", "smb1.ttf")
 
+    makeLuaSprite('progressbar', 'pause/progressbar', 0, 0)
+    scaleObject("progressbar", 6, 6)
+    screenCenter('progressbar', 'X')
+    setProperty('progressbar.y', 284)
+    setProperty('progressbar.x', getProperty("progressbar.x") -4.05)
+
+    makeLuaSprite('mxStand', 'pause/stand', 0, 0)
+    scaleObject("mxStand", 6, 6)
+    setProperty('mxStand.x', getProperty("progressbar.x"))
+    setProperty('mxStand.y', (getProperty("progressbar.y") - getProperty("mxStand.height")) + (getProperty("progressbar.height") / 2))
+
+    makeLuaSprite('selector', 'pause/selector', 0, 0)
+    scaleObject("selector", 6, 6)
+    screenCenter('selector', 'X')
+
     table.insert(pauseMenuSprs,"substateBG")
+
+    local curY = 372
+
+    for i,v in ipairs(options) do
+        local sprName = tostring(string.lower(v).."Text")
+        makeLuaText(sprName, v, 200, 448.1, curY)
+        scaleObject(sprName, 3, 3)
+        setTextFont(sprName, "smb1.ttf")
+        setTextAlignment(sprName,"left")
+        curY = curY + 81
+
+        table.insert(pauseMenuSprs,sprName)
+    end
+
+    setProperty("selector.x",(getProperty("continueText.x") - (getProperty("continueText.height") / 2) - getProperty("selector.height")))
+    setProperty("selector.y",(getProperty("continueText.y") + (getProperty("selector.width") / 4)))
+    
     table.insert(pauseMenuSprs,"pauseText")
     table.insert(pauseMenuSprs,"songnameText")
+    table.insert(pauseMenuSprs,"progressbar")
+    table.insert(pauseMenuSprs,"mxStand")
+    table.insert(pauseMenuSprs,"selector")
 
     for i,v in ipairs(pauseMenuSprs) do
         setObjectCamera(v, 'other')
         setProperty(tostring(v..".antialiasing"),valse)
-        setProperty(tostring(v..".visible"),false)
-        addLuaSprite(v,true)
     end
+end
+
+local function updatePauseSelection()
+    local optsLen = 0
+    for i,_ in ipairs(options) do
+        optsLen = i
+    end
+    if selectedOption < 1 then
+        selectedOption = optsLen
+    elseif selectedOption > optsLen then
+        selectedOption = 1
+    end
+    local pauseOption = tostring(string.lower(options[selectedOption]).."Text")
+    setProperty("selector.x",(getProperty(tostring(pauseOption)..".x") - (getProperty(tostring(pauseOption)..".height") / 2) - getProperty("selector.height")))
+    setProperty("selector.y",(getProperty(tostring(pauseOption)..".y") + (getProperty("selector.width") / 4)))
 end
 
 -- Custom script to change Boyfriend's and Opponent's strum notes in FNF Psych Engine
@@ -566,14 +627,56 @@ end
 
 function onCustomSubstateUpdate(name, elapsed)
     if name == "customPause" then
+        local progress = getSongPosition() / songLength
+
+        local diff = getProperty('progressbar.width') / 16
+        local startX = getProperty('progressbar.x') + diff -- posición inicial
+        local width = getProperty('progressbar.width') - diff -- ancho total de la barra
+    
+        setProperty('mxStand.x', startX + (progress * width))
+        if keyJustPressed("up") then
+            selectedOption = selectedOption - 1
+        elseif keyJustPressed("down") then
+            selectedOption = selectedOption + 1
+        end
+
+        if keyJustPressed("up") or keyJustPressed("down") then
+            playSound("menu_select")
+            updatePauseSelection()
+        end
+
         -- Check if the player presses the Space key to exit the substate
-        if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.ENTER') then
-            for i,v in ipairs(pauseMenuSprs) do
-                setProperty(tostring(v..".visible"),false)
+        if keyJustPressed('accept') then
+            local optionText = tostring(string.lower(options[selectedOption]))
+            if optionText == "continue" then
+                for i,v in ipairs(pauseMenuSprs) do
+                    removeLuaSprite(v)
+                end
+                playSound("pause")
+                closeCustomSubstate() -- Close the substate and return to the main game
+            elseif optionText == "retry" then
+                playSound("stomp")
+                restartSong()
+            elseif optionText == "options" then
+                playSound("stomp")
+                -- THANKS YOU SO MUCH TABI REVIVAL MOD, your tha best!
+                runHaxeCode([[
+                import options.OptionsState;
+                import backend.MusicBeatState;
+                game.paused = true;
+                game.vocals.volume = 0;
+                MusicBeatState.switchState(new OptionsState());
+                if (ClientPrefs.data.pauseMusic != 'None') {
+                    FlxG.sound.playMusic(Paths.music("freakyMenu"), game.modchartSounds('pauseMusic').volume);
+                    FlxTween.tween(FlxG.sound.music, {volume: 1}, 0.8);
+                    FlxG.sound.music.time = game.modchartSounds('pauseMusic').time;
+                }
+                OptionsState.onPlayState = true;
+                ]])
+            elseif optionText == "end" then
+                playSound("coin")
+                exitSong()
             end
-            closeCustomSubstate() -- Close the substate and return to the main game
-        elseif getPropertyFromClass('flixel.FlxG', 'keys.justPressed.ESCAPE') then
-            exitSong(true)
         end
     elseif name == "endpipe" then
         inGameBoyfriendX = inGameBoyfriendX - (runningspeed/4)
@@ -584,12 +687,21 @@ function onCustomSubstateUpdate(name, elapsed)
     end
 end
 
+function onSoundFinished(tag)
+    if tag == 'music' then
+        playSound("breakfast", 0.8, "music")
+    end
+end
+
 function onPause()
     if songStarted then
         if not keyPressed("Left") or not keyPressed("Right") or not keyPressed("Up") or not keyPressed("Down") then
+            initPauseMenu()
             for i,v in ipairs(pauseMenuSprs) do
-                setProperty(tostring(v..".visible"),true)
+                addLuaSprite(v,true)
             end
+            playSound("pause")
+            updatePauseSelection()
             openCustomSubstate('customPause', true)
             -- Create elements of the substate when it is initialized
         end
@@ -655,7 +767,7 @@ function onStepHit()
     if curStep == 1255 then
         camerabffollow = false
         innocenceDoesntGetYouFar()
-    elseif curStep == 1262 or curStep == 1264 or curStep == 1265 or curStep == 1269 then
+    elseif curStep == 1262 or curStep == 1264 or curStep == 1265 or curStep == 1267 then
         popupx = popupx + 960
     elseif curStep == 1530 then
         setOnLuas("mxrunning", false)
